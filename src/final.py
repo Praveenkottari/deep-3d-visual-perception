acc84c31bfebb859d84fbefb4579683886367ab8
airl010 <kottaripraveen3004@gmail.com>
2025-04-22 12:22:06 +0530

    updated code/created training code

--- src/final.py
+++ src/final.py
@@ -30,29 +30,16 @@ from heads.detection_head import *
 
 
 # detection model
-model = detection_model()
-
-T_velo_cam2 = np.array([
-    [607.48, -718.54, -10.188, -95.573],
-    [180.03, 5.8992, -720.15, -93.457],
-    [0.99997, 0.00048595, -0.0072069, -0.28464]
-])
-
-# Convert to 4x4 homogeneous transformation matrix
-T_homogeneous = np.vstack([T_velo_cam2, [0, 0, 0, 1]])
-
-# Compute inverse
-T_came2_velo = np.linalg.inv(T_homogeneous)
-
+# model = detection_model()
 
 def main():
     
     configs = parse_demo_configs()
 
-    configs.dataset_dir = "/home/airl010/1_Thesis/visionNav/fusion/dataset/2011_10_03_drive_0027_sync/"
+    configs.dataset_dir = "/home/airl010/1_Thesis/visionNav/fusion/dataset/2011_10_03_drive_0047_sync/"
 
     model3d = create_model(configs)
-    print('\n\n' + '-*=' * 30 + '\n\n')
+    print('\n\n' + '-*' * 60 + '\n\n')
     assert os.path.isfile(configs.pretrained_path), "No file at {}".format(configs.pretrained_path)
     model3d.load_state_dict(torch.load(configs.pretrained_path, map_location='cpu'))
     print('Loaded weights from {}\n'.format(configs.pretrained_path))
@@ -65,14 +52,13 @@ def main():
 
     out_cap = None
     demo_dataset = Demo_KittiDataset(configs)
-     
 
-    draw_boxes =True
+
+    draw_boxes =False
     with torch.no_grad():
         for sample_idx in range(len(demo_dataset)):
             
             metadatas, front_bevmap, back_bevmap, img_rgb = demo_dataset.load_bevmap_front_vs_back(sample_idx)
-            # cv2.imshow("demo",cv2.COLOR_BGR2RGB(img_rgb))
             front_detections, front_bevmap, fps = do_detect(configs, model3d, front_bevmap, is_front=True)
             back_detections, back_bevmap, _ = do_detect(configs, model3d, back_bevmap, is_front=False)
 
@@ -92,89 +78,61 @@ def main():
 
             # merge front and back bevmap
             full_bev = np.concatenate((back_bevmap, front_bevmap), axis=1)
+
+            img_path = metadatas['img_path'][0]
             img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+            cv2.imshow("img_bgr", img_bgr)
 
 
+            # detections = model(img_bgr)
 
-            detections = model(img_bgr)
-
-            # Filter detections based on confidence and class indices
-            desired_classes = [0, 1, 2, 3, 5, 7]  # Only person, bicycle, car, motorcycle, bus, truck
-            confidence_threshold = 0.5
-            filtered_boxes = []
-            for box in detections[0].boxes.data.cpu().numpy():  # [x1, y1, x2, y2, confidence, class]
-                confidence, cls = box[4], int(box[5])
-                if confidence >= confidence_threshold and cls in desired_classes:
-                    filtered_boxes.append(box)
+            # # Filter detections based on confidence and class indices
+            # desired_classes = [0, 1, 2, 3, 5, 7]  # Only person, bicycle, car, motorcycle, bus, truck
+            # confidence_threshold = 0.5
+            # filtered_boxes = []
+            # for box in detections[0].boxes.data.cpu().numpy():  # [x1, y1, x2, y2, confidence, class]
+            #     confidence, cls = box[4], int(box[5])
+            #     if confidence >= confidence_threshold and cls in desired_classes:
+            #         filtered_boxes.append(box)
             
-            filtered_boxes = np.array(filtered_boxes)
+            # filtered_boxes = np.array(filtered_boxes)
             
-            # Draw boxes on the image
-            if draw_boxes:
-                if len(filtered_boxes) > 0:
-                    for box in filtered_boxes:
-                        x1, y1, x2, y2, conf, cls = box
-                        label = f"{model.names[int(cls)]} {conf:.2f}"
-                        # Draw rectangle and label on the image
-                        image = cv2.rectangle(img_bgr, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
-                        image = cv2.putText(img_bgr, label, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
+            # # Draw boxes on the image
+            # if draw_boxes:
+            #     if len(filtered_boxes) > 0:
+            #         for box in filtered_boxes:
+            #             x1, y1, x2, y2, conf, cls = box
+            #             label = f"{model.names[int(cls)]} {conf:.2f}"
+            #             # Draw rectangle and label on the image
+            #             # image = cv2.rectangle(img_bgr, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+            #             image = cv2.putText(img_bgr, label, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
         
-                else:
-                    print("No detections met the criteria.")
+            #     else:
+            #         print("No detections met the criteria.")
 
 
             calib = Calibration(configs.calib_path)
-
-
-
-
             kitti_dets = convert_det_to_real_values(front_detections)
             if len(kitti_dets) > 0:
                 kitti_dets[:, 1:] = lidar_to_camera_box(kitti_dets[:, 1:], calib.V2C, calib.R0, calib.P2)
                 img_bgr = show_rgb_image_with_boxes(img_bgr, kitti_dets, calib)
             img_bgr = cv2.resize(img_bgr, (cnf.BEV_WIDTH * 2, 375))
             
-
-            _,left_image = demo_dataset.get_image(sample_idx)
-            bin_path = demo_dataset.get_lidar_path(sample_idx)
-
-            left_image = cv2.cvtColor(left_image, cv2.COLOR_RGB2BGR)
-
- 
-            bboxes, velo_uvz = get_detection_coordinates(left_image, bin_path, model,T_velo_cam2)
-            
-            velo_image = draw_velo_on_image(velo_uvz, left_image)
-            cv2.imshow("projection",velo_image)
-
-
-
-
-            uvz = bboxes[:, -3:]
-            #lidar co ordinate for detected obejcts 
-            canvas_out = draw_scenario(uvz,T_came2_velo,line_draw=True)
-            cv2.imshow("dist",canvas_out)
-
-
-
-  
-
             out_img = np.concatenate((img_bgr, full_bev), axis=0)
-            write_credit(out_img, (50, 410), text_author='', org_fps=(900, 410), fps=fps)
+            cv2.putText(out_img, 'Speed: {:.1f} FPS'.format(fps), org=(900, 400), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1,  color = (255, 255, 255), thickness = 2)
 
-            # Create the video writer if not already created
-            if out_cap is None:
-                out_cap_h, out_cap_w = out_img.shape[:2]
-                fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-                out_path = os.path.join(configs.results_dir, '{}lid_cam_off_road.avi'.format(configs.foldername))
-                print('Create video writer at {}'.format(out_path))
-                out_cap = cv2.VideoWriter(out_path, fourcc, 15, (out_cap_w, out_cap_h))
-
+            # # Create the video writer if not already created
+            # if out_cap is None:
+            #     out_cap_h, out_cap_w = out_img.shape[:2]
+            #     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            #     out_path = os.path.join(configs.results_dir, '{}lid_cam_off_road.avi'.format(configs.foldername))
+            #     print('Create video writer at {}'.format(out_path))
+            #     out_cap = cv2.VideoWriter(out_path, fourcc, 15, (out_cap_w, out_cap_h))
             # Write the output frame to the video
-            out_cap.write(out_img)
+            #out_cap.write(out_img)
 
             # DISPLAY IN REAL TIME
-            # --------------------
-            cv2.imshow("SFA3D Demo", out_img)
+            cv2.imshow("Demo", out_img)
             key = cv2.waitKey(1) & 0xFF
             # If you want to stop early by pressing 'q'
             if key == ord('q'):
@@ -183,9 +141,6 @@ def main():
     if out_cap:
         out_cap.release()
     cv2.destroyAllWindows()
-
-
-
 
 
 if __name__ == '__main__':
